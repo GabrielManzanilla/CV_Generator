d1b69bfe04be921c6733b96f2cd95bc05cd4b77d
GabrielManzanilla <gabriel29444@gmail.com>
2025-04-04 16:44:04 -0600

    Feat: Creacion del apartado de vistas del feedback

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def feedback(request):
+	# SOlicitud nuevamente para verificacion y mejoracion
+
+	#llamado a la descarga
+
+	
+	# impresion de los resultados del feedback
+	return render(request, 'feedback.html')
