4d672b11b8bdf757bd0ef1172b8f7c83da6f2af5
DZEL <dzel@kali.samuel>
2025-04-05 07:24:49 +0200

    working in auth app

--- login/templates/paginainicio.js
+++ login/templates/paginainicio.js
@@ -3,14 +3,14 @@ import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/fire
 import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC0cT5NSwaVEea2alSgbY0VIpkFvCdKZ88",
-    authDomain: "cv-generator1.firebaseapp.com",
-    projectId: "cv-generator1",
-    storageBucket: "cv-generator1.firebasestorage.app",
-    messagingSenderId: "450931232331",
-    appId: "1:450931232331:web:3b9c0ee2210553420147ce",
-    measurementId: "G-17RBTHEXTP"
-};
+    apiKey: "AIzaSyB9SxFqkYR55cEuGuXsVIbZBm8DBTp5td4",
+    authDomain: "cvgenerator-99c85.firebaseapp.com",
+    projectId: "cvgenerator-99c85",
+    storageBucket: "cvgenerator-99c85.appspot.com",
+    messagingSenderId: "252413914431",
+    appId: "1:252413914431:web:36db117534a61dde6bb78a",
+    measurementId: "G-H0EZ1YXT2V"
+  };
  
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
