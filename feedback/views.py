8e7d0919d175ac8d280b887f2d987345fbf26c72
GabrielManzanilla <gabriel29444@gmail.com>
2025-04-04 19:12:54 -0600

    fixed merge function feedback and request_info

--- feedback/views.py
+++ feedback/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def feedback(request):
+def show_feedback(request):
 	# SOlicitud nuevamente para verificacion y mejoracion
 
-	#llamado a la descarga
-
-	
+	#llamado a la descargs
 	# impresion de los resultados del feedback
 	return render(request, 'feedback.html')
+
+def fix_feedback(request):
+	#llamado a la descargs
+	# impresion de los resultados del feedback
+	return render(request, 'fix_feedback.html')
--- request_info/views.py
+++ request_info/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def request_info(request):
+		if request.method == "POST":
+				# Process the form data
+				personal_info = request.POST.get('personal_info')
+				fixed_info = request.POST.get('personal_fixes')
+				vacancy_info = request.POST.get('vacancy_info')
+				# Add more fields as needed
+
+				# Here you can save the data to the database or perform other actions
+
+				#return render(request, 'request_info/success.html', {'name': name})
+
+		return render(request, 'form_page.html')
